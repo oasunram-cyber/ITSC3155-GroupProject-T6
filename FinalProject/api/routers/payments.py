cfddc76cc4f685f7cb1460562eb5fa750e9dad90
Samobp <164976450+Samobp@users.noreply.github.com>
2025-12-02 21:12:07 -0500

    Fixed payments.py

--- FinalProject/api/routers/payments.py
+++ FinalProject/api/routers/payments.py
@@ -1,27 +1,8 @@
-from fastapi import APIRouter, Depends, status
-from sqlalchemy.orm import Session
-from controllers import payments as controller
-from schemas import payments as schema
-from dependencies.database import get_db
-
-router = APIRouter(
-    tags=['Payments'],
-    prefix="/payments"
-)
-
-@router.post("/", response_model=schema.Payment)
-def create_payment(request: schema.PaymentCreate, db: Session = Depends(get_db)):
-    return controller.create(db=db, request=request)
-
-@router.get("/{item_id}", response_model=schema.Payment)
-def read_payment(item_id: int, db: Session = Depends(get_db)):
-    return controller.read_one(db, item_id=item_id)
-
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from dependencies.database import get_db
-from controllers import payment as controller
+from controllers import payments as controller
 from schemas import payments as schema
 
 router = APIRouter(
@@ -29,18 +10,14 @@ router = APIRouter(
     tags=["payments"],
 )
 
-
 @router.post("/", response_model=schema.Payment)
 def create_payment(request: schema.PaymentCreate, db: Session = Depends(get_db)):
     return controller.create(db, request)
-
 
 @router.get("/{payment_id}", response_model=schema.Payment)
 def get_payment(payment_id: int, db: Session = Depends(get_db)):
     return controller.read_one(db, payment_id)
 
-
 @router.get("/by-order/{order_id}", response_model=schema.Payment)
 def get_payment_by_order(order_id: int, db: Session = Depends(get_db)):
     return controller.read_by_order_id(db, order_id)
-
